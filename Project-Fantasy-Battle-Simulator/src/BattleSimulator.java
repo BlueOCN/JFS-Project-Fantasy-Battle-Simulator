43f86319d2bed5f1afe658377c02b24b45479125
BlueOCN <robgonzalezmu@gmail.com>
2025-05-05 18:31:39 -0600

    Update - Mage unique stats added.

--- Project-Fantasy-Battle-Simulator/src/BattleSimulator.java
+++ Project-Fantasy-Battle-Simulator/src/BattleSimulator.java
@@ -1,5 +1,5 @@
 
-public class Main {
+public class BattleSimulator {
     public static void main(String[] args) {
         // Main
     }
---
+++ Project-Fantasy-Battle-Simulator/src/Mage.java
@@ -0,0 +1,20 @@
+public class Mage {
+
+    private String id;
+
+    // Governs spell power and mana pool
+    private int intelligence;
+
+    // Speeds up magic recovery over time
+    private int manaRegeneration;
+
+    // Enhances damage from fire, ice, lightning, or other magical elements
+    private int elementalMastery;
+
+    // Provides magical shielding from attacks
+    private int arcaneBarrier;
+
+    // Increases the efficiency and potency of spells
+    private int spellChanneling;
+
+}
